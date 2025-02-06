3cd6a4a844983d52aa6229c320ab82d86953b1cd
Missing Textures <dariormt@gmail.com>
2025-02-06 19:52:40 +0100

    Added basic player animations

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -4,16 +4,16 @@ namespace game {
 void Player::Init() {
   Character::Init();
   
-  transform.relativeScale = 3;
+  transform.relativeScale = glm::vec2(3);
 }
 
 void Player::Start() {
   Character::Start();
   
-  auto anim = GET_ANIMATION->LoadTextureAtlas("assets/Idle.json");
+  auto anim = GET_ANIMATION->LoadTextureAtlas("assets/TestAnimPlayer.json");
   m_animComp->SetTextureAtlas(anim);
-  m_animComp->SetCurrentAnim("Default");
-  SetTexture("assets/Idle.png");
+  m_animComp->SetCurrentAnim("Idle");
+  SetTexture("assets/TestAnimPlayer.png");
   m_animComp->PlayAnim();
   
   m_controllerComponent = std::make_unique<PlayerController>(this);
@@ -22,7 +22,20 @@ void Player::Start() {
 
 void Player::Update(double delta) {
   Character::Update(delta);
-  if (m_controllerComponent) m_controllerComponent->Update();
+  if (m_controllerComponent)
+    m_controllerComponent->Update();
+
+  if (velocity.x != 0 || velocity.y != 0) {
+    m_animComp->SetCurrentAnim("Walk");
+  }else {
+    m_animComp->SetCurrentAnim("Idle");
+  }
+
+  if (velocity.x < 0) {
+    transform.relativeScale.x = -3;
+  } else {
+    transform.relativeScale.x = 3;
+  }
 }
 
 void Player::Destroy() {}
