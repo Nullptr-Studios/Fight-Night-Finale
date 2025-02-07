00317f155cd72a9423306b6a8bf333044805a1c6
Xein <xgonip@gmail.com>
2025-02-07 23:45:28 +0100

    Started with Enemies

---
+++ src/Enemies/Enemy.cpp
@@ -0,0 +1,48 @@
+#include "Enemy.hpp"
+#include "Player/Player.hpp"
+#include "glm/geometric.hpp"
+
+namespace game {
+
+void Enemy::Serialize() {
+  Character::Serialize();
+  m_distanceToAttack = j["distanceToAttack"];
+}
+
+void Enemy::Start() {
+  m_state = FOLLOW;
+}
+
+bool Enemy::OnCollision(Sigma::Collision::CollisionEvent& e) {
+  Character::OnCollision(e);
+  return false;
+}
+
+void Enemy::Update(double delta) {
+  switch (m_state) {
+    default: m_state = FOLLOW;
+    case FOLLOW: OnFollow(delta); break;
+    case ATTACK: OnAttack(delta); break;
+    case DEAD: OnDead(delta); break;
+  }
+}
+
+void Enemy::OnFollow(double delta) {
+  glm::vec3 targetPosition = m_player->transform.position - transform.position;
+  targetPosition.y = targetPosition.z; // Removes player jump -x
+
+  if (glm::vec2(targetPosition.x, targetPosition.y).length() < m_distanceToAttack)
+    m_state = ATTACK;
+
+  targetPosition = glm::normalize(targetPosition);
+  Move( {targetPosition.x, targetPosition.y} );
+}
+
+void Enemy::OnAttack(double delta) {
+  BasicAttack();
+}
+
+void Enemy::OnDead(double delta) { }
+
+}
+
---
+++ src/Enemies/Enemy.hpp
@@ -0,0 +1,53 @@
+/**
+ * @file Enemy.hpp
+ * @author Xein
+ * @date 07/02/25
+ *
+ * @brief [Brief description of the file's purpose]
+ */
+#pragma once
+#include <pch.hpp>
+#include "Collision/CollisionEvent.hpp"
+#include "Objects/Character.hpp"
+
+namespace game {
+class Player;
+
+class Enemy : public Sigma::Character {
+  enum EnemyState {
+    FOLLOW = 0,
+    ATTACK,
+    DEAD
+  };
+
+public:
+  explicit Enemy(const Sigma::id_t id) : Character(id) {}
+
+  void Init() override;
+  void Start() override;
+  void Update(double delta) override;
+  void Destroy() override;
+
+  void Serialize() override;
+
+  bool OnCollision(Sigma::Collision::CollisionEvent& e) override;
+  // bool OnDamage(Sigma::Damage::DamageEvent& e) override;
+
+  void SetPlayer(Player* player) { m_player = player; }
+
+protected:
+  void SetState(EnemyState state) { m_state = state; }
+  EnemyState GetState() { return m_state; }
+
+private:
+  Player* m_player = nullptr;
+
+  EnemyState m_state = FOLLOW;
+  void OnFollow(double delta);
+  void OnAttack(double delta);
+  void OnDead(double delta);
+
+  float m_distanceToAttack = 0.0f;
+};
+
+}
