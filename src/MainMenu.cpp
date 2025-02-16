036c4a20ec1c16708c1055d430faf57e98ea1549
Dante Harper <dantedigdog@gmail.com>
2025-02-16 19:23:15 +0100

    progress bar works

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -5,13 +5,19 @@
 #include "Objects/Camera.hpp"
 #include "UI/PlayButton.hpp"
 #include "UI/QuitButton.hpp"
+#include "UI/UIProgressBar.hpp"
 
 void game::MainMenu::Load() {
   Scene::Load();
   GET_CAMERA->SetCurrentCamera(GET_FACTORY->CreateObject<Sigma::Camera>("Main Camera"));
+
   auto playButton = GET_FACTORY->CreateObject<game::PlayButton>("Play Button");
   playButton->transform.position.x += 200;
-  
+
   auto quitButton = GET_FACTORY->CreateObject<game::QuitButton>("Quit Button");
   quitButton->transform.position.x -= 200;
+
+  auto progress = GET_FACTORY->CreateObject<Sigma::UIProgressBar>("Progress");
+  progress->transform.scale = {400,100};
+  progress->m_progress = 1;
 }
