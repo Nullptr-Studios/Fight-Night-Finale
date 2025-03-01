f77af912bf0570382a311ea9f9a818d8679497ed
Alexey <agurov@digipen.edu>
2025-03-01 01:18:02 +0100

    Mistakes fixed!

--- src/Testing_Scene.cpp
+++ src/Testing_Scene.cpp
@@ -34,14 +34,14 @@ void Testing_Scene::Load() {
   AddChild(GET_CAMERA->GetCurrentCamera());
 
   floor = GET_FACTORY->CreateObject<Sigma::Actor>();
-  floor->SetTexture("assets/level-1/scene-4-floor.png");
-  floor->transform.scale = {933.0f, 273.0f};
+  floor->SetTexture("assets/level-1/scene-5-floor.png");
+  floor->transform.scale = {1485.0f, 333.0f};
   floor->transform.position.z = -5000;
   AddChild(floor);
 
   walls = GET_FACTORY->CreateObject<Sigma::Actor>();
-  walls->SetTexture("assets/level-1/scene-4-walls.png");
-  walls->transform.scale = {933.0f, 273.0f};
+  walls->SetTexture("assets/level-1/scene-5-walls.png");
+  walls->transform.scale = {1485.0f, 333.0f};
   walls->transform.position.z = -5000;
   AddChild(walls);
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,20 @@
 #include "GameManager.hpp"
 #include "Testing_Scene.hpp"
 #include "UI/MainMenu.hpp"
+#include "PrototypeScene.hpp"
+
+//SCENES
+#include "level1/Scene1.hpp"
+#include "level1/Scene2.hpp"
+#include "level1/Scene3.hpp"
+#include "level1/Scene4.hpp"
+#include "level1/Scene5.hpp"
+#include "level1/Scene6.hpp"
+#include "level1/Scene7.hpp"
 
 int main() {
   std::unique_ptr<Sigma::GameManager> gameManager = std::make_unique<Sigma::GameManager>("Game", 800, 600);
-  auto scene = new game::Testing_Scene("Prototype", 0, "assets/level-1/scene-4.json");
+  auto scene = new game::Scene7("Cornelius Coconut", 69, "assets/level-1/scene-7.json");
   gameManager->LoadScene(scene);
 
   AEGfxSetFullscreen(false);
