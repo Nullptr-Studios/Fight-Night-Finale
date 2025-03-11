88980438a65745f25c14cff6f5add472c7595e54
Dante Harper <dantedigdog@gmail.com>
2025-03-11 14:32:16 +0100

    bug fix

--- src/Enemies/Tnt.cpp
+++ src/Enemies/Tnt.cpp
@@ -6,24 +6,30 @@
 #include "aecore/imgui/imgui.h"
 #include "Collision/OneHitCollider.hpp"
 #include "DamageSystem/DamageEvent.hpp"
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/compatibility.hpp>
 namespace game {
 
 void Tnt::Init() {
+  m_start = transform.position;
+  m_target = {100,0,0};
   m_attackCollider = GET_FACTORY->CreateObject<Sigma::Collision::OneHitCollider>("Attack Collider");
   m_attackCollider->GetCollider()->enabled = false;
   m_boomBox = {100,100,100};
   m_powBox = {300,300};
   m_damage = 30;
+  m_timer = 0;
 }
 
 void Tnt::Update(double delta) {
   if (boom) {
     return;
   }
-  m_timer += (float)delta;
   if (m_timer >= 8) {
     boom = true;
     m_attackCollider->Do(transform.position, m_boomBox, m_damage, this, Sigma::Damage::DamageType::DAMAGE,m_powBox, true);
+  } else {
+    m_timer += (float)delta;
   }
 
 }
@@ -36,7 +42,8 @@ void Tnt::DebugWindow() {
       m_attackCollider->Do(transform.position, m_boomBox, m_damage, this, Sigma::Damage::DamageType::DAMAGE,m_powBox, true);
     }
     if (ImGui::Button("Launch")) {
-      m_attackCollider->Do(transform.position, m_boomBox, m_damage, this, Sigma::Damage::DamageType::DAMAGE,m_powBox, true);
+      m_timer = 0;
+      transform.position = m_start;
     }
   }
 }
@@ -46,3 +53,4 @@ void Tnt::Destroy() {
 }
 }
 
+#undef GLM_USE_EXPERIMENTAL
--- src/Enemies/Tnt.hpp
+++ src/Enemies/Tnt.hpp
@@ -14,10 +14,12 @@ class Tnt : public Sigma::Actor {
 private:
   float m_timer = 0;
   bool boom = false;
-  glm::vec3 m_boomBox;
-  glm::vec2 m_powBox;
-  float m_damage;
+  glm::vec3 m_boomBox{};
+  glm::vec2 m_powBox{};
+  float m_damage{};
   Sigma::Collision::OneHitCollider *m_attackCollider{};
+  glm::vec3 m_start{};
+  glm::vec3 m_target{};
 public:
   explicit Tnt(Sigma::id_t id) : Actor(id) {}
   void Init() override;
