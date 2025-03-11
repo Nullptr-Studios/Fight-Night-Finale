8a011144f3335e92b58218caf7d97b66c63685b0
Dante Harper <dantedigdog@gmail.com>
2025-03-11 01:18:38 +0100

    im not insane your insane

---
+++ src/Enemies/Tnt.cpp
@@ -0,0 +1,42 @@
+#include "Tnt.hpp"
+#include "Core.hpp"
+#include "Collision/OneHitCollider.hpp"
+#include "Factory.hpp"
+#include "Collision/Collider.hpp"
+#include "aecore/imgui/imgui.h"
+#include "Collision/OneHitCollider.hpp"
+#include "DamageSystem/DamageEvent.hpp"
+namespace game {
+
+void Tnt::Init() {
+  m_attackCollider = GET_FACTORY->CreateObject<Sigma::Collision::OneHitCollider>("Attack Collider");
+  m_attackCollider->GetCollider()->enabled = false;
+  m_boomBox = {100,100,100};
+  m_powBox = {300,300};
+  m_damage = 50;
+}
+
+void Tnt::Update(double delta) {
+  if (boom) {
+    return;
+  }
+  m_timer += (float)delta;
+  if (m_timer >= 8) {
+    boom = true;
+    m_attackCollider->Do(transform.position, m_boomBox, m_damage, this, Sigma::Damage::DamageType::DAMAGE,m_powBox, true);
+
+  }
+
+}
+
+void Tnt::DebugWindow() {
+  if (ImGui::Button("Boom")) {
+    m_attackCollider->Do(transform.position, m_boomBox, m_damage, this, Sigma::Damage::DamageType::DAMAGE,m_powBox, true);
+  }
+}
+void Tnt::Destroy() {
+  if (m_attackCollider != nullptr)
+    GET_FACTORY->DestroyObject(m_attackCollider);
+}
+}
+
