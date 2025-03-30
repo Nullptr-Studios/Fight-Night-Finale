571cd1f4b0533b31013c54e4d439922e3da58d31
Dante Harper <dantedigdog@gmail.com>
2025-03-30 17:53:27 +0200

    fukcing kinda works but im kms

--- src/Enemies/TntEnemy.cpp
+++ src/Enemies/TntEnemy.cpp
@@ -1,6 +1,9 @@
 #include "TntEnemy.hpp"
 #include "Random.hpp"
 #include "Enemies/Tnt.hpp"
+#include "glm/trigonometric.hpp"
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/rotate_vector.hpp"
 
 namespace game {
 void TntEnemy::Init() {
@@ -24,11 +27,36 @@ void TntEnemy::Init() {
 void TntEnemy::FollowState() {
   if (!m_isDoingSomething) return;
   isAvoiding = false;
-  m_animComp->SetCurrentAnim("Walk");
   glm::vec3 targets[2];
+
   targets[0] = m_nearest->transform.GetDepthPosition()+ glm::vec3(-200,0,0);
   targets[1] = m_nearest->transform.GetDepthPosition()+ glm::vec3(200,0,0);
   m_position = ((m_distance.x >= 0)?targets[0]:targets[1]);
+  if (!m_sceneBoundsPoly->IsPointInside((glm::vec2) m_position)) {
+    m_position = ((m_distance.x >= 0) ? targets[1] : targets[0]);
+  }
+  if (!m_sceneBoundsPoly->IsPointInside((glm::vec2) m_position)) {
+    targets[0] = m_nearest->transform.GetDepthPosition() + glm::vec3(0, -200, 0);
+    targets[1] = m_nearest->transform.GetDepthPosition() + glm::vec3(0, 200, 0);
+    m_position = ((m_distance.x >= 0) ? targets[1] : targets[0]);
+  }
+  if (!m_sceneBoundsPoly->IsPointInside((glm::vec2) m_position)) {
+    m_position = ((m_distance.x >= 0) ? targets[0] : targets[1]);
+  }
+
+  while(!m_sceneBoundsPoly->IsPointInside((glm::vec2) m_position)) {
+    Sigma::Random::Circle();
+    m_position = (glm::vec2)m_nearest->transform.position + (Sigma::Random::Circle() * 200.0f);
+  }
+
+  if (!m_sceneBoundsPoly->IsPointInside((glm::vec2) m_position)) {
+    m_timer = 0.1;
+    m_nextState = STATE_FOLLOW;
+    SetState(STATE_PAUSED);
+    return;
+  }
+
+  m_animComp->SetCurrentAnim("Walk");
 
   if (fabs(m_distance.y) <= 200 && fabs(m_distance.x) <= 200 && fabs(m_distance.x) >= 150) {
     m_timer = Sigma::Random::Float(.2f, .3f);
